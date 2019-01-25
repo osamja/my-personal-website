0cb795cea80cad46a41e8d6dba3fe7ef877cce8d
Sammy Javed <sammyjaved@gmail.com>
2019-01-25 03:16:32 +0000

    Made devops changes. need to add .env in prod

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const port_number = Number.parseInt(process.argv[2], 10)
+console.log("Port: " + port_number);
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/books', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public/html', 'books.html'));
-});
+if (Number.isInteger(port_number)) {
+	app.listen(port_number);
+} else {
+	console.log("Invalid port number");
+}
 
-app.listen(9000);
