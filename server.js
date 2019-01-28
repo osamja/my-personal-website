269f4cdbaaeb3266393cd16b18d773de0f58e07c
Sammy Javed <sammyjaved@gmail.com>
2019-01-28 05:31:18 +0000

    Added path for ssl verification

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ console.log("Port: " + port_number);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/.well-known/pki-validation/9F2F8C2065C8A044C6CE2027826598AA.txt', function (req, res) {
+  res.sendfile('/.well-known/pki-validation/9F2F8C2065C8A044C6CE2027826598AA.txt')
+});
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/.well-known/pki-validation/file.txt', function (req, res) {
+  res.sendfile('/.well-known/pki-validation/file.txt')
+});
+
 if (Number.isInteger(port_number)) {
 	app.listen(port_number);
 } else {
