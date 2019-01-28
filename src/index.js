b5b2981c2d58363463a2d0489ccf486c510eeed3
Sammy Javed <sammyjaved@gmail.com>
2019-01-27 17:53:07 -0800

    wrote server redirect code from osamjaved to sammyjaved

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+let currentLocation = window.location;
+
+console.log('Hostname: ' + currentLocation);
+
+if (currentLocation.hostname === "http://osamjaved.com") {
+    window.location.replace('http://sammyjaved.com');
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
