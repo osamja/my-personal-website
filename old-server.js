62b051e5de6fa3b95d435da51d2d07e17005dfc9
Sammy Javed <sammyjaved@gmail.com>
2019-01-29 04:24:58 +0000

    added old server code for redirect

---
+++ old-server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port_number = Number.parseInt(process.argv[2], 10)
+app.use(express.static(path.join(__dirname, 'build')));
+
+if (Number.isInteger(port_number)) {
+	app.listen(port_number);
+} else {
+	console.log("Invalid port number");
+}
+
