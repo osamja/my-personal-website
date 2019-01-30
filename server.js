67c36a26f12ae7fef1ac71e45aa3788006797ad8
Sammy Javed <sjaved@adobe.com>
2019-01-29 18:36:29 -0800

    wip

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const path = require('path');
 const https = require('https');
+const http = require('http');
 const app = express();
 const fs = require('fs');
 
 const port_number = Number.parseInt(process.argv[2], 10)
 
 const options = {
-    ca: fs.readFileSync('/home/sammy/config/sammyjaved_com/sammyjaved_com.ca-bundle'),
-    key: fs.readFileSync('/home/sammy/config/sammyjaved_com/example_com.key'),
-    cert: fs.readFileSync('/home/sammy/config/sammyjaved_com/sammyjaved_com.crt'),
+    ca: fs.readFileSync('Config/sammyjaved_com/sammyjaved_com.ca-bundle'),
+    key: fs.readFileSync('Config/sammyjaved_com/example_com.key'),
+    cert: fs.readFileSync('Config/sammyjaved_com/sammyjaved_com.crt'),
 };
 
 
@@ -25,7 +26,15 @@ app.get('/.well-known/pki-validation/file.txt', function (req, res) {
   res.sendfile('/.well-known/pki-validation/file.txt')
 });
 
+// sammy express middlewhere
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now())
+  next()
+});
+
 if (Number.isInteger(port_number)) {
+	console.log("Creating server at port " + port_number);
+	http.createServer(app).listen(3004);
 	https.createServer(options, app).listen(port_number);
 } else {
 	console.log("Invalid port number");
