3282b55dba75eb0ba325f7cbe455db8bd65693f9
Sammy Javed <sjaved@adobe.com>
2019-02-01 16:52:00 -0800

    Added a local server file for testing. Removed http support with current server file

--- localServer.js
+++ localServer.js
@@ -4,14 +4,13 @@ const https = require('https');
 const app = express();
 const fs = require('fs');
 
-const port_number = Number.parseInt(process.argv[2], 10)
+const port_number = 8005;// Number.parseInt(process.argv[2], 10)
 
 const options = {
     ca: fs.readFileSync('Config/sammyjaved_com/sammyjaved_com.ca-bundle'),
     key: fs.readFileSync('Config/sammyjaved_com/example_com.key'),
     cert: fs.readFileSync('Config/sammyjaved_com/sammyjaved_com.crt'),
 };
-
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -26,7 +25,6 @@ app.get('/.well-known/pki-validation/file.txt', function (req, res) {
 });
 
 if (Number.isInteger(port_number)) {
-	console.log("Creating server at port " + port_number);
 	https.createServer(options, app).listen(port_number);
 } else {
 	console.log("Invalid port number");
