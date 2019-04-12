6d4cd264bce48bc34612456f4edb01195ec85617
Sammy Javed <sjaved@adobe.com>
2019-04-12 07:10:47 -0700

    fixed local routing

--- src/scripts/Routes/images.js
+++ src/scripts/Routes/images.js
@@ -1,10 +1,13 @@
-// const process = require('process');
-// require('dotenv').config();
-
 const BASE_DOMAIN = 'sammyjaved.com';
 const PROTOCOL = 'https://';
 const BASE_PATH = 'static';
 const CATEGORY = 'images';
+
+/**
+ * React apps can only read env variables prefixed with 'REACT_APP'.
+ * process.env.environment does not work locally with npm start, needs server
+ */
+
 
 /**
  * Returns the base domain used for retrieving static images
@@ -12,7 +15,7 @@ const CATEGORY = 'images';
  * @returns {string}
  */
 function getBaseDomain() {
-  if (process.env.REACT_APP_ENV === 'local') {
+  if (process.env.REACT_APP_ENV === 'local' && process.env.environment === "local") {
     return 'localhost:8002';
   } else if (process.env.REACT_APP_ENV === 'development') {
       return BASE_DOMAIN + ':8001';
@@ -30,7 +33,7 @@ function getBaseDomain() {
  * @returns {string}
  */
 export function getBaseUrl() {
-  if (process.env.REACT_APP_ENV) {
+  if (process.env.REACT_APP_ENV !== 'local' ) {
       return PROTOCOL + getBaseDomain();
   } else {
       return 'http://' + getBaseDomain();
