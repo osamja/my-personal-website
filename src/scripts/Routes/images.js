7a4cca05eee235435138a94f9ee5dd90edc61341
Sammy Javed <sjaved@adobe.com>
2019-07-29 21:03:06 -0700

    trying to migrate site to use cloudinary

--- src/scripts/Routes/images.js
+++ src/scripts/Routes/images.js
@@ -40,6 +40,10 @@ export function getBaseUrl() {
   }
 }
 
+export function getCloudinaryBaseUrl() {
+  return 'https://res.cloudinary.com/dgrlg424p/image/upload/';
+}
+
 /**
  * Return base url for my personal website
  * @returns {string}
@@ -48,6 +52,8 @@ export function getBasePath() {
   const basePath = BASE_PATH + '/' + CATEGORY;
   return basePath;
 }
+
+
 
 export const DEFAULT_BLACK_IMAGE_PATH = getBaseUrl() + '/' + getBasePath() + '/general/colors/black.jpg';
 export const DEFAULT_BLACK_THUMBNAIL_IMAGE_PATH = getBaseUrl() + '/' + getBasePath() + '/general/colors/black-thumbnail.jpg';
