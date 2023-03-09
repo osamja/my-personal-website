68d50d2b234981462049a4d9ab044f038522cc83
Sammy Javed <sammyjaved@gmail.com>
2023-03-09 10:29:17 +0000

    updated homepage content; removed typekit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,7 +8,7 @@ export default function Homepage() {
         font-weight: 250;
     `;
 
-    const StyledParagraph = styled.p`
+    const StyledLi = styled.li`
         font-weight: 100;
     `;
 
@@ -19,13 +19,15 @@ export default function Homepage() {
 
     return (
         <div>
-            <div className="Homepage" style={{color: "#e0e0e0", fontWeight: 200, textAlign: 'center'}}>
+            <div className="Homepage" style={{color: "#e0e0e0", fontWeight: 200}}>
                 {/* <Gallery gridData={imgUrls} /> */}
-                <StyledHeader>What's Up!</StyledHeader>
-                <StyledParagraph>
-                    I'm Sammy, a software developer from the Bay Area.  I attended Evergreen Valley College and UC Berkeley.  I studied computer science.  I enjoy coding, playing guitar, PC gaming which these days is mostly Rocket League, watching movies, playing basketball, drinking coffee, video editing, watching sports (go Warriors and 49ers) and hiking.  I'm currently self-employed looking to pursue some creative endeavors.  Before that, I worked at Adobe for four years.  Io vivo a San Jose, California, USA.
-                </StyledParagraph>
-                <StyledImage src={getHomePageSource("IMG_2464.JPG")} alt="here is an AI generated portrait of me" />      
+                <StyledHeader>Adab</StyledHeader>
+                <ul>
+                    <StyledLi>I am currently studying ML abroad (in Kyoto, Japan as of March 2023) since I never got the chance to do it in school.</StyledLi>
+                    <StyledLi>I was a full-stack web developer at Adobe for 4 years where I worked on the Adobe Stock website (Feb 2018 - April 2022)</StyledLi>
+                    <StyledLi>I did my undergraduate studies in computer science from UC Berkeley (May 2015 - Dec 2017)</StyledLi>
+                </ul>
+                <StyledImage src={getHomePageSource("anime-backpack-me.JPG")} alt="here is an AI generated portrait of me" />      
 
                 <div className="Homepage" style={{color: "#e0e0e0", fontWeight: 200}}>
                     <Gallery gridData={imgUrls} />
--- src/scripts/Gallery/homepage.js
+++ src/scripts/Gallery/homepage.js
@@ -20,34 +20,34 @@ export const imgUrls = [
     //     'text': 'Journal',
     //     'url': '/journal',
     // },
-    {
-        'src': getHomePageSource('projects.jpg'),
-        'text': 'Projects',
-        'url': '/projects',
-    },
-    {
-        'src': getHomePageSource('books.jpg'),
-        'text': 'Books',
-        'url': '/books',
-    },
-    {
-        'src': getHomePageSource('travel.jpg'),
-        'text': 'Travel',
-        'url': '/travel',
-    },
-    {
-        'src': getHomePageSource('guitar-body.jpg'),
-        'text': 'Guitar',
-        'url': '/guitar',
-    },
-    {
-        'src': getHomePageSource('movies.jpg'),
-        'text': 'Movies',
-        'url': '/movies',
-    },
-    {
-        'src': getHomePageSource('compressed/evc2.jpg'),
-        'text': 'Live Events',
-        'url': '/shows',
-    },
+    // {
+    //     'src': getHomePageSource('projects.jpg'),
+    //     'text': 'Projects',
+    //     'url': '/projects',
+    // },
+    // {
+    //     'src': getHomePageSource('books.jpg'),
+    //     'text': 'Books',
+    //     'url': '/books',
+    // },
+    // {
+    //     'src': getHomePageSource('travel.jpg'),
+    //     'text': 'Travel',
+    //     'url': '/travel',
+    // },
+    // {
+    //     'src': getHomePageSource('guitar-body.jpg'),
+    //     'text': 'Guitar',
+    //     'url': '/guitar',
+    // },
+    // {
+    //     'src': getHomePageSource('movies.jpg'),
+    //     'text': 'Movies',
+    //     'url': '/movies',
+    // },
+    // {
+    //     'src': getHomePageSource('compressed/evc2.jpg'),
+    //     'text': 'Live Events',
+    //     'url': '/shows',
+    // },
 ];
