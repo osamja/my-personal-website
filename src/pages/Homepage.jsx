15ac1266d1ec5f3d21b95aefae7279b44ebbbdcc
Sammy Javed <me@sammyjaved.com>
2023-03-27 17:53:29 +0900

    added nostr and github icons

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Bio from './Bio';
+import {getHomePageSource} from '../scripts/Gallery/homepage';
 
 export default function Homepage() {
 
@@ -8,8 +9,68 @@ export default function Homepage() {
         color: pink;
     `;
 
+    const nostrPath = getHomePageSource("nostr-link.png");
+    const githubPath = getHomePageSource("github-mark/github-mark-white-link.png");
+
+    const slideIn = keyframes`
+        0% {
+            transform: translateX(-100%);
+            opacity: 0;
+        }
+        100% {
+            transform: translateX(0);
+            opacity: 1;
+        }
+    `;
+
+    const StyledNostrLink = styled.img`
+        // Center the image
+        margin-left: auto;
+        margin-right: auto;
+        border-radius: 50%;
+        // Set the border
+        border: 1px solid #fff;
+        // Set the box-shadow
+        box-shadow: 0 0 0 1px #fff;
+        // Set the transition
+        transition: all 0.3s ease-in-out;
+        // Set the hover state
+        &:hover {
+            // Set the border
+            border: 1px solid #fff;
+            // Set the box-shadow
+            box-shadow: 0 0 0 1px #fff;
+            // Set the transform
+            transform: scale(1.1);
+        }
+
+    `;
+
+    const StyledSocialLinks = styled.a`
+        display: inline-flex;
+        animation: ${slideIn} 0.5s ease-out forwards;
+    `;
+
+    const StyledContainer = styled.div`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: nowrap;
+        gap: 1rem;
+    `;
+
     return (
         <div>
+            <StyledContainer>
+                <StyledSocialLinks href="https://snort.social/p/npub16wgk7s474eppd9xwqwfccvh57u0ev7c9q69sr4cnk3s3vk7av3rqf98had">
+                    <StyledNostrLink src={nostrPath} alt='Image' />
+                </StyledSocialLinks>
+                
+                <StyledSocialLinks href='https://github.com/osamja'>
+                    <StyledNostrLink src={githubPath} alt='Image' />
+                </StyledSocialLinks>    
+            </StyledContainer>
+
             <Bio />
             <ul style={{color: 'white'}}>
                 <li>In 2022, I backpacked Europe for 4 months.  Here's a <StyledLink href="https://www.youtube.com/watch?v=wDBnOPnfvyM">video</StyledLink> I made of my trip.</li>
