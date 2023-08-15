a01985e3d5830fcee2d647eb5dcbeddc899a2332
Sammy Javed <sammyjaved@gmail.com>
2023-08-15 18:53:42 +0000

    adding pyaar ai to bio timeline

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import {getHomePageSource} from '../scripts/Gallery/homepage';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { getHomePageSource } from '../scripts/Gallery/homepage';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 
 import 'react-vertical-timeline-component/style.min.css';
 
@@ -27,11 +27,11 @@ export default function Bio() {
     );
 
     const SchoolIcon = () => (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M21 17v-6.9L12 15L1 9l11-6l11 6v8h-2Zm-9 4l-7-3.8v-5l7 3.8l7-3.8v5L12 21Z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M21 17v-6.9L12 15L1 9l11-6l11 6v8h-2Zm-9 4l-7-3.8v-5l7 3.8l7-3.8v5L12 21Z" /></svg>
     );
 
     const TravelIcon = () => (
-        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><g fill="currentColor"><path d="M29.8 17.396a159.482 159.482 0 0 1-11.583 0a6 6 0 1 0 11.567 0h.016ZM25 30.341l5 1.488V40h-5v-9.659ZM23 40h-5v-8.17l5-1.489V40Z"/><path fillRule="evenodd" d="M48 0H0v48h48V0ZM24 6c-4.5 0-7 1.2-7 1.2V12l-3.529 3.529c-.593.593-.236 1.588.6 1.648c.59.042 1.299.087 2.12.13l-.011-.001a8 8 0 1 0 15.64 0a112.94 112.94 0 0 0 2.109-.13c.836-.06 1.193-1.054.6-1.647L30.999 12V7.2S28.5 6 24 6Zm.288 22.042l6.542 1.947l5.607-3.816A1 1 0 0 1 38 27v5h-2v-3.11l-4 2.722V40c0 .768.289 1.47.764 2H15.236c.475-.53.764-1.232.764-2v-8.465l-4-2.666V32h-2v-5a1 1 0 0 1 1.555-.832l5.696 3.797l6.46-1.923A.979.979 0 0 1 24 28a.998.998 0 0 1 .288.042ZM37 34a3 3 0 0 0-3 3v2a3 3 0 0 0 3 3h5v-5a3 3 0 0 0-3-3h-2ZM9 34a3 3 0 0 0-3 3v5h5a3 3 0 0 0 3-3v-2a3 3 0 0 0-3-3H9Z" clipRule="evenodd"/></g></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><g fill="currentColor"><path d="M29.8 17.396a159.482 159.482 0 0 1-11.583 0a6 6 0 1 0 11.567 0h.016ZM25 30.341l5 1.488V40h-5v-9.659ZM23 40h-5v-8.17l5-1.489V40Z" /><path fillRule="evenodd" d="M48 0H0v48h48V0ZM24 6c-4.5 0-7 1.2-7 1.2V12l-3.529 3.529c-.593.593-.236 1.588.6 1.648c.59.042 1.299.087 2.12.13l-.011-.001a8 8 0 1 0 15.64 0a112.94 112.94 0 0 0 2.109-.13c.836-.06 1.193-1.054.6-1.647L30.999 12V7.2S28.5 6 24 6Zm.288 22.042l6.542 1.947l5.607-3.816A1 1 0 0 1 38 27v5h-2v-3.11l-4 2.722V40c0 .768.289 1.47.764 2H15.236c.475-.53.764-1.232.764-2v-8.465l-4-2.666V32h-2v-5a1 1 0 0 1 1.555-.832l5.696 3.797l6.46-1.923A.979.979 0 0 1 24 28a.998.998 0 0 1 .288.042ZM37 34a3 3 0 0 0-3 3v2a3 3 0 0 0 3 3h5v-5a3 3 0 0 0-3-3h-2ZM9 34a3 3 0 0 0-3 3v5h5a3 3 0 0 0 3-3v-2a3 3 0 0 0-3-3H9Z" clipRule="evenodd" /></g></svg>
     );
 
     const StyledLink = styled.a`
@@ -55,10 +55,25 @@ export default function Bio() {
     `;
 
     return (
-        <div className="Bio" style={{color: '#e0e0e0', textAlign: 'left', margin: '5%'}}>
+        <div className="Bio" style={{ color: '#e0e0e0', textAlign: 'left', margin: '5%' }}>
             <div>
-                <div className="Homepage" style={{color: "#e0e0e0", fontWeight: 200}}>
+                <div className="Homepage" style={{ color: "#e0e0e0", fontWeight: 200 }}>
                     <VerticalTimeline>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--travel"
+                            date="2022 - present"
+                            contentStyle={{ background: 'rgb(202, 156, 158)', color: 'white' }}
+                            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                            iconStyle={{ background: 'rgb(202, 156, 158)', color: 'white' }}
+                            icon={<TravelIcon />}
+                        >
+                            <h3 className="vertical-timeline-element-title">Founder</h3>
+                            <StyledTimelinePicture src={getHomePageSource("anime-backpack-me.JPG")} alt="here is an AI generated portrait of me" />
+                            <h4 className="vertical-timeline-element-subtitle">pyaar.ai</h4>
+                            <p>
+                                I'm building a startup that uses AI to help people be more musical.
+                            </p>
+                        </VerticalTimelineElement>
                         <VerticalTimelineElement
                             className="vertical-timeline-element--travel"
                             date="2022 - present"
@@ -97,8 +112,8 @@ export default function Bio() {
                             iconStyle={{ background: 'rgb(25, 65, 113)', color: 'rgb(237, 184, 72)' }}
                             icon={<SchoolIcon />}
                         >
-                            <h3 className="vertical-timeline-element-title" style={{display: 'inline'}}>B.A. in Computer Science</h3>
-                            <StyledTimelinePicture src={getHomePageSource("berkeley-seal.png")} alt="UC Berkeley" />      
+                            <h3 className="vertical-timeline-element-title" style={{ display: 'inline' }}>B.A. in Computer Science</h3>
+                            <StyledTimelinePicture src={getHomePageSource("berkeley-seal.png")} alt="UC Berkeley" />
                             <h4 className="vertical-timeline-element-subtitle">Berkeley, CA</h4>
                             <p>
                                 Took as many CS courses as I could handle + a Harry Potter class.
